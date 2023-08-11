bc6f5b93c1c0acdc0032c8a43d80d4fd9e3a3570
Vladislava <heshtegvlada@gmail.com>
2023-08-11 19:27:51 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const itemsGallery = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsGallery);
+
+galleryContainer.addEventListener('click', onImgClick);
+
+
+function onImgClick (event) {
+    event.preventDefault();
+    console.log(event.target.nodeName);
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src = ${event.target.dataset.source} width="800" height="600">
+`);
+
+  instance.show();
+  
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const itemsGallery = galleryItems.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+
+
+    galleryContainer.insertAdjacentHTML('beforeend', itemsGallery);
+
+    galleryContainer.addEventListener('click', onImgClick);
+
+
+function onImgClick (event) {
+    event.preventDefault();
+    console.log(event.target.nodeName);
+
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+ });
